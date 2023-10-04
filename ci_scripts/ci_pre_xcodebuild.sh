51422b0ca1bc5b232bb3766fe8b9217188695dbd
Toni Sevener <tsevener@wikimedia.org>
2023-10-04 13:39:50 -0500

    Adjust scripts for injecting environment vars on Experimental build

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -15,14 +15,14 @@ EntitlementsFile="${CI_PRIMARY_REPOSITORY_PATH}/Wikipedia/Wikipedia.entitlements
 if [[ ${CI_WORKFLOW} == "Weekly Staging Build" ]]; then
     InfoPListFile="${CI_PRIMARY_REPOSITORY_PATH}/Wikipedia/Staging-Info.plist"
     ./copy_environment_vars.sh $EntitlementsFile $InfoPListFile $MERCHANT_ID $PAYMENTS_API_KEY
-    echo "Execute copy merchant IDs."
-    exit 0
-    
+
+elif [[ ${CI_WORKFLOW} == "Wikipedia Experimental PR Build" ]]; then
+    InfoPListFile="${CI_PRIMARY_REPOSITORY_PATH}/Wikipedia/Experimental-Info.plist"
+    ./copy_environment_vars.sh $EntitlementsFile $InfoPListFile $MERCHANT_ID $PAYMENTS_API_KEY
+
 elif [[ ${CI_WORKFLOW} == "Nightly Build" ]]; then
     InfoPListFile="${CI_PRIMARY_REPOSITORY_PATH}/Wikipedia/Wikipedia-Info.plist"
     ./copy_environment_vars.sh $EntitlementsFile $InfoPListFile $MERCHANT_ID $PAYMENTS_API_KEY
 fi
-
-    
 
 exit 0
